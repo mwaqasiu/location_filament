b140d717f2963148b2bb3c90b7d814c042025829
Muhammad Waqas <mwaqasiu@gmail.com>
2023-07-09 02:41:53 +0500

    register package name in service provider

--- src/LocationFilamentServiceProvider.php
+++ src/LocationFilamentServiceProvider.php
@@ -1,6 +1,14 @@
 <?php
+namespace CentoSquare\LocationFilament;
 
-class LocationFilamentServiceProvider
+use Filament\Navigation\UserMenuItem;
+use Filament\PluginServiceProvider;
+use Spatie\LaravelPackageTools\Package;
+
+class LocationFilamentServiceProvider extends PluginServiceProvider
 {
-
+    public function configurePackage(Package $package): void
+    {
+        $package->name('location_filament');
+    }
 }
